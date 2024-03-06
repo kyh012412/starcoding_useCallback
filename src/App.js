2a28c7267007003f6ec9aff3804aec8531a351d8
kyh012412 <kyh0124.12@gmail.com>
2024-03-06 17:45:55 +0900

    someFunction이 계속 변경되는 상태

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [number,setNumber] = useState(0);
@@ -9,13 +9,18 @@ function App() {
     console.log(`someFunc: number: ${number}`);
     return;
   }
+
+  useEffect(()=>{
+    console.log("someFunction이 변경되었습니다.");
+  },[someFunction])
+
   return (
-   <div>
-    <input type="number" value={number}
-    onChange={(e)=> setNumber(e.target.value)} />
-    <br />
-    <button onClick={someFunction}>Call someFunc</button>
-   </div>
+    <div>
+      <input type="number" value={number}
+      onChange={(e)=> setNumber(e.target.value)} />
+      <br />
+      <button onClick={someFunction}>Call someFunc</button>
+    </div>
   );
 }
 
