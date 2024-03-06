eb4b38ed646cf919a08a728ff35b2b2ad3153fa2
kyh012412 <kyh0124.12@gmail.com>
2024-03-06 18:59:57 +0900

    올바른 useCallback상태 number가 바뀌면 Callback을 갱신하지만 toggle이 바뀔때는 메모이제이션이 사용됨

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { useCallback, useEffect, useState } from 'react';
 
 function App() {
   const [number,setNumber] = useState(0);
+  const [toggle,setToggle] = useState(true);
 
   const someFunction = useCallback(() => {
     console.log(`someFunc: number: ${number}`);
     return;
-  },[number])
+  },[number]);
 
   useEffect(()=>{
     console.log("someFunction이 변경되었습니다.");
@@ -17,7 +17,8 @@ function App() {
   return (
     <div>
       <input type="number" value={number}
-      onChange={(e)=> setNumber(e.target.value)} />
+      onChange={(e)=> setNumber(parseInt(e.target.value))} />
+      <button onClick={()=>{setToggle(!toggle)}}>{toggle.toString()}</button>
       <br />
       <button onClick={someFunction}>Call someFunc</button>
     </div>
